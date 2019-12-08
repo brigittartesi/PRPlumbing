3972adb3cbb3c6b68a8512572c1f3ff6fb6198ca
Doruk Guner <dorukgunerr@gmail.com>
2019-12-08 19:08:24 +1100

    Updates

---
+++ precache-manifest.782134b3772ca36933c04d2ae82ef292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910ed37caec9948d39994f8f79fc17d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27db4abcf948c40785a",
+    "url": "/static/css/main.14a09805.chunk.css"
+  },
+  {
+    "revision": "3042616045a1b10fd5e2",
+    "url": "/static/js/2.c7fd4b51.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c7fd4b51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c27db4abcf948c40785a",
+    "url": "/static/js/main.ac31cad5.chunk.js"
+  },
+  {
+    "revision": "9351b516e8693ffe587c",
+    "url": "/static/js/runtime-main.15857a81.js"
+  }
+]);
