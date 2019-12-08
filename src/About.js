4fea0032d3972abbe2894a3e12c1a683a7875bf4
Doruk Guner <dorukgunerr@gmail.com>
2019-12-08 19:26:15 +1100

    first try

---
+++ src/About.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import './About.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class About extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>About</h2> <br />
+            </div>
+        );
+    }
+
+}
+
+export default About;
